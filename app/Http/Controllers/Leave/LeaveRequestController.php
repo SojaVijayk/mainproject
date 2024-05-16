5d84774a5c31ba7d2d927eea25b5f0cf34758f0a
jasarjasu786 <jasarjasu786@gmail.com>
2024-05-16 12:45:56 +0530

    contract period update error exception added

--- app/Http/Controllers/Leave/LeaveRequestController.php
+++ app/Http/Controllers/Leave/LeaveRequestController.php
@@ -23,6 +23,7 @@ use Maatwebsite\Excel\Facades\Excel;
 use PDF;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\MasterFunctionController;
+use Illuminate\Support\Facades\Redirect;
 
 class LeaveRequestController extends Controller
 {
@@ -36,7 +37,11 @@ class LeaveRequestController extends Controller
 
     $leave_types = Leave::orderBy('id','DESC')->get();
 
-
+    if($employee_details->contract_start_date == NULL ){
+      // return Redirect::back()->withErrors(['insufficient' => 'Contract Period Not Updated']);
+      $message = 'Contract Period Not Updated. Please contact our Human Resources (HR) department at hr@cmd.kereal.gov.in to update your contract period. Once this is done, you can proceed with your request as usual.';
+      return view('content.pages.pages-custom-error',compact('message'),['pageConfigs'=> $pageConfigs]);
+    }
 
 
      $leaves_total_credit = LeaveAssign::select('leave_assigns.id','leave_assigns.leave_type as leave_type_id','leaves.leave_type','total_credit','employment_types.employment_type','leave_assigns.status','leave_assigns.created_at')
