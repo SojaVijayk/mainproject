7ab86fff843768b4fa17bb5cc8c25d33bf8840f1
jasarjasu786 <jasarjasu786@gmail.com>
2024-05-17 11:06:53 +0530

    leave page error handler added

--- app/Http/Controllers/Leave/LeaveRequestController.php
+++ app/Http/Controllers/Leave/LeaveRequestController.php
@@ -37,7 +37,7 @@ class LeaveRequestController extends Controller
 
     $leave_types = Leave::orderBy('id','DESC')->get();
 
-    if($employee_details->contract_start_date == NULL ){
+    if($employee_details->contract_start_date == NULL && $employee_details->employment_type !=1  && $employee_details->employment_type !=3){
       // return Redirect::back()->withErrors(['insufficient' => 'Contract Period Not Updated']);
       $message = 'Contract Period Not Updated. Please contact our Human Resources (HR) department at hr@cmd.kereal.gov.in to update your contract period. Once this is done, you can proceed with your request as usual.';
       return view('content.pages.pages-custom-error',compact('message'),['pageConfigs'=> $pageConfigs]);
