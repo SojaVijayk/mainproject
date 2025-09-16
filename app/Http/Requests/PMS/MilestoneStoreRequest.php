a0d3fcff820222332030f50134c1fee9d2324b32
jasarjasu786 <jasarjasu786@gmail.com>
2025-09-16 19:33:11 +0530

    milestone date validation removed

--- app/Http/Requests/PMS/MilestoneStoreRequest.php
+++ app/Http/Requests/PMS/MilestoneStoreRequest.php
@@ -16,7 +16,8 @@ class MilestoneStoreRequest extends FormRequest
         return [
             'name' => 'required|string|max:255',
             'description' => 'nullable|string',
-            'start_date' => 'required|date|after_or_equal:today',
+            // 'start_date' => 'required|date|after_or_equal:today',
+             'start_date' => 'required|date',
             'end_date' => 'required|date|after_or_equal:start_date',
             'weightage' => 'required|numeric|min:0|max:100',
             'invoice_trigger' => 'nullable|boolean',
